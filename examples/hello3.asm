2d39768058f930962acec7192c6d94e798fbe49b
Leandro Vital <leandro.santos@ima.sp.gov.br>
2024-03-18 10:51:05 -0300

    Update hello3.asm

--- examples/hello3.asm
+++ examples/hello3.asm
@@ -32,7 +32,7 @@ Print:
 	ld a,(hl)	; Load the byte from memory at address indicated by HL to A.
 	and a	; Same as CP 0 but faster.
 	ret z	; Back behind the call print if A = 0
-	call	CHPUT	; Call the routine to display a character.
+	call CHPUT	; Call the routine to display a character.
 	inc hl	; Increment the HL value.
 	jr Print	; Relative jump to the address in the label Print.
 
